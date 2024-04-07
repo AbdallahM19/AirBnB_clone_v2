9c22d2ea1b5f3bed88bde986003f92dce625fbe0
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-04-07 12:52:17 -0700

    airbnb_3

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,27 +2,23 @@
 """
 Fabric script that deletes out-of-date archives
 """
-
-from fabric.api import *
 import os
-from datetime import datetime
+from fabric.api import *
 
 env.hosts = ['54.226.45.35', '100.25.46.237']
 
 
 def do_clean(number=0):
-    """
-    Deletes out-of-date archives
-    """
+    """Deletes out-of-date archives"""
     number = 1 if int(number) == 0 else int(number)
 
-    archive = sorted(os.listdir("versions"))
-    [archive.pop() for i in range(number)]
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archive]
+        [local("rm ./{}".format(i)) for i in archives]
 
     with cd("/data/web_static/releases"):
-        archive = run("ls -tr").split()
-        archive = [a for a in archive if "web_static_" in a]
-        [archive.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archive]
+        archives = run("ls -tr").split()
+        archives = [i for i in archives if "web_static_" in i]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(i)) for i in archives]
