860c25b083659d0f8a0cef70c21953eb834934f2
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-04-07 14:54:27 -0700

    airbnb_3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,26 +6,25 @@ using the function deploy:
 """
 
 from fabric.api import *
-from time import strftime
 from os.path import exists, basename
 from datetime import datetime
 
-
 env.hosts = ['54.226.45.35', '100.25.46.237']
-
-
 def do_pack():
     """
     Packs all files in web_static to versions folder with name
     web_static_<year><month><day><hour><minute><second>.tgz
     """
-    a = 'web_static_' + strftime("%Y%m%d%H%M%S") + '.tgz'
-    run('mkdir -p versions')
-    file = local("tar -czvf versions/{} web_static".format(a))
-    if file is not None:
-        return "versions/{}".format(a)
-    else:
+    try:
+        time = datetime.now().strftime("%Y%m%d%H%M%S")
+        if not exists('versions'):
+            local("mkdir -p versions")
+        file_name = "versions/web_static_{}.tgz".format(time)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except Exception as e:
         return None
+
 
 def do_deploy(archive_path):
     """
@@ -40,6 +39,7 @@ def do_deploy(archive_path):
             archive_name
         )
         put(archive_path, '/tmp/{}'.format(archive_file))
+        run("rm -rf {}/".format(archive_folder))
         run('mkdir -p {}/'.format(archive_folder))
         run('tar -xzf /tmp/{} -C {}/'.format(
             archive_file, archive_folder
@@ -57,10 +57,8 @@ def do_deploy(archive_path):
     except Exception as e:
         return False
 
-
 def deploy():
     """Full deployment process"""
     a_path = do_pack()
     if not a_path:
         return False
-    return do_deploy(a_path)
