78947cdea9bda5ef9b4cfb9908d228faad4dfa4a
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-04-07 12:59:38 -0700

    airbnb_3

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -10,21 +10,15 @@ env.hosts = ['54.226.45.35', '100.25.46.237']
 
 def do_clean(number=0):
     """Deletes out-of-date archives"""
-    number = int(number)
-    if number == 0 or number == 1:
-        number_to_keep = 1
-    else:
-        number_to_keep = number
+    number = 1 if int(number) == 0 else int(number)
 
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
     with lcd("versions"):
-        local_archives = sorted(os.listdir("."))
-        archives_to_remove = local_archives[:-number_to_keep]
-        for archive in archives_to_remove:
-            local("rm -f {}".format(archive))
+        [local("rm ./{}".format(i)) for i in archives]
 
     with cd("/data/web_static/releases"):
-        remote_archives = run("ls -tr").split()
-        remote_archives = [archive for archive in remote_archives if "web_static_" in archive]
-        archives_to_remove = remote_archives[:-number_to_keep]
-        for archive in archives_to_remove:
-            run("rm -rf {}".format(archive))
+        archives = run("ls -tr").split()
+        archives = [i for i in archives if "web_static_" in i]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(i)) for i in archives]
