024be00f20ab7303f301222831ba5266d0974b2a
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-04-19 15:26:56 -0700

    flask

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,7 +4,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 import urllib.parse
-
 from models.base_model import BaseModel, Base
 from models.state import State
 from models.city import City
@@ -41,16 +40,18 @@ class DBStorage:
         objects = dict()
         all_classes = (User, State, City, Amenity, Place, Review)
         if cls is None:
-            for class_type in all_classes:
-                query = self.__session.query(class_type)
+            for i in all_classes:
+                query = self.__session.query(i)
                 for obj in query.all():
-                    obj_key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-                    objects[obj_key] = obj
+                    objects[
+                        '{}.{}'.format(obj.__class__.__name__, obj.id)
+                    ] = obj
         else:
             query = self.__session.query(cls)
             for obj in query.all():
-                obj_key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-                objects[obj_key] = obj
+                objects[
+                    '{}.{}'.format(obj.__class__.__name__, obj.id)
+                ] = obj
         return objects
 
     def delete(self, obj=None):
@@ -79,12 +80,12 @@ class DBStorage:
     def reload(self):
         """Loads storage database"""
         Base.metadata.create_all(self.__engine)
-        SessionFactory = sessionmaker(
+        Sess_Factory = sessionmaker(
             bind=self.__engine,
             expire_on_commit=False
         )
-        self.__session = scoped_session(SessionFactory)()
+        self.__session = scoped_session(Sess_Factory)()
 
     def close(self):
         """Closes the storage engine."""
-        self.__session.close()
+        self.__session.remove()
