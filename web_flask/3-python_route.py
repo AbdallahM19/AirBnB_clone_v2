59d964cec7d16f9595b5302be72b9aa1f7a869c8
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-04-18 21:21:23 -0700

    flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,6 +22,7 @@ def C_text(text):
     """Displays 'HBNB'"""
     return "C {}".format(text.replace('_', ' '))
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text='is_cool'):
