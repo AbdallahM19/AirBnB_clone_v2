29a97069252b73bafda4fdedc2442afdf3462f9a
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-04-19 15:50:34 -0700

    flask

--- models/state.py
+++ models/state.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import os
+from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
 from models.base_model import BaseModel, Base
 from models.city import City
 
@@ -13,8 +12,8 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(
         String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    ) if getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship(
             'City',
             cascade='all, delete, delete-orphan',
