220a9f7056ab3eebc9de45d1934e16d12c6d1bf8
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-04-21 15:10:04 -0700

    flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,9 +6,9 @@ from models.state import State
 from models import storage
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-@app.route('/states_list')
+
+@app.route('/states_list', strict_slashes = False)
 def states_list():
     """/states_list: display a HTML page: (inside the tag BODY:"""
     state_all = list(storage.all().values())
@@ -16,7 +16,7 @@ def states_list():
     return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
+@app.teardown_appcontext(strict_slashes = False)
 def teardown(exception):
     """Declare a method to handle @app.teardown_appcontext"""
     storage.close()
