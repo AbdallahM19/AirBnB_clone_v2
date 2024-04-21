3614df87c428c2f2cbf56d515b4c2a3774a879d8
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-04-20 17:50:06 -0700

    flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Write a script that starts a Flask web application"""
+"""
+Script that starts a Flask web application
+"""
 
 from flask import Flask, render_template
-from models.state import State
 from models import storage
 
 app = Flask(__name__)
@@ -10,18 +11,22 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown(exception):
-    """Declare a method to handle @app.teardown_appcontext"""
+    """
+    Remove current SQLAlchemy Session
+    """
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """/states_list: display a HTML page: (inside the tag BODY:"""
-    state_all = list(storage.all(State).values())
-    states = sorted(state_all, key=lambda i: i.name)
+    """
+    Display a HTML page: (inside the tag BODY)
+    - H1 tag: “States”
+    - LI tag: description of one State: <state.id>: <B><state.name></B>
+    """
+    states = sorted(storage.all("State").values(), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
-    """run it"""
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
