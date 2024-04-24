09bf28773932c48eef374522258832af5fc30939
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-04-24 16:46:11 -0700

    last update for flash / airbnb webframe

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,16 +11,15 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """/states_list: display a HTML page: (inside the tag BODY:"""
-    states = storage.all(State)
+    states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """Declare a method to handle @app.teardown_appcontext"""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
 
-if __name__ == "__main__":
-    """run it"""
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
