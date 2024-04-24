f793d211d570f6156fd82c7e489ebe91cefcb355
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-04-24 16:50:43 -0700

    last update for flash / airbnb webframe

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Write a script that starts a Flask web application"""
+"""Write a script that starts a Flask web application:"""
 
 from flask import Flask, render_template
 from models.state import State
 from models import storage
+
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """/states_list: display a HTML page: (inside the tag BODY:"""
+    """/states_list: display a HTML page:(inside the tag BODY)"""
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
